d4f38df4e301c9008342f3613c1f6723d54b73e3
MaggieWalsh <miggieholland@gmail.com>
2022-05-09 10:17:27 +0000

    Create html pages and link to .py file using render_template

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import os
-from flask import Flask
-
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -9,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(
