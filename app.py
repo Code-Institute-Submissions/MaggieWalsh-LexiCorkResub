c8d67e6e457c97c122aa120ddc05de0d9e78bbbb
MaggieWalsh <miggieholland@gmail.com>
2022-05-19 10:39:56 +0000

    Update app.py file with user submit function

--- app.py
+++ app.py
@@ -20,26 +20,31 @@ mongo = PyMongo(app)
 
 alphabetList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+
 @app.route("/")
 @app.route("/index")
 def index():
+    ''' Adds letters to the home page '''
     return render_template(
-        'index.html', page_title="Browse the dictionary", alphabetList=alphabetList)
+        'index.html', page_title="Browse the dictionary", 
+        alphabetList=alphabetList)
 
-# Try to copy this function to create a new page for words to appear on and link to alphabet
+
 @app.route("/alphabet/<letter>")
 def alphabet(letter):
+    ''' Adds letters to the alphabet page '''
     words = mongo.db.dictionary
-    foundWords = list(words.find({"category" : letter.lower()}))
+    found_words = list(words.find({"category" : letter.lower()}))
     print(letter)
     return render_template(
-        'alphabet.html', page_title=letter, alphabetList=alphabetList, words=foundWords)
+        'alphabet.html', page_title=letter, alphabetList=alphabetList, words=found_words)
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    ''' Adds registration function '''
     if request.method == "POST":
-        # check if username already exists in db
+        # check if username exists
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
@@ -53,7 +58,6 @@ def register():
         }
         mongo.db.users.insert_one(register)
 
-        # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
         return redirect(url_for("profile", username=session["user"]))
@@ -63,6 +67,7 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    ''' Creates login function '''
     if request.method == "POST":
         # check if username exists in db
         existing_user = mongo.db.users.find_one(
@@ -92,6 +97,7 @@ def login():
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
+    ''' Dynamically creates a page for the user '''
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
@@ -104,17 +110,55 @@ def profile(username):
 
 @app.route("/logout")
 def logout():
+    ''' Logs user out '''
     # remove user from session cookie
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html", page_title="Contact Us")
+# Submit new word
+@app.route("/submit_word", methods=["GET", "POST"])
+def submit_word():
+    ''' Submit new word '''
+    # if user is in session, submit new word
+    if request.method == "GET":
+        new_word_value = request.args.get('new_word')
+        if new_word_value is not None:
+            new_word_value = new_word_value.lower()
+
+    if request.method == "POST":
+        existing_word = mongo.db.dictionary.find_one(
+            {"word": request.form.get("word").lower()})
+
+        if not existing_word:
+            word = {
+                "word": request.form.get("word").lower(),
+                "category": request.form.get("category"),
+                "definition": request.form.get("definition")
+            }
+            mongo.db.dictionary.insert_one(word)
+        
+        # if new word is added successfully, redirect user to dashboard
+            flash(
+                "Word added successfully. Once reviewed by the editors,\
+                     it will display in the dictionary")
+            return redirect(url_for("index"))
+
+        # if the word already exists, redirect to new instance of the form
+        # and inform the word exists
+        else:
+            flash("This word already exists, please submit a new word")
+            return redirect(url_for("submit_word"))
+
+    categories = mongo.db.dictionary.find()
+    return render_template(
+        "submit_word.html",
+        categories=categories,
+        new_word=new_word_value)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+
