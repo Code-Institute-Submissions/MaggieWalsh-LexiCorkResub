7a20cf86e8c9c623bcf472a836a1582d79e84386
MaggieWalsh <miggieholland@gmail.com>
2022-05-22 13:30:14 +0000

    Tidy files

--- app.py
+++ app.py
@@ -32,18 +32,15 @@ def index():
 @app.route("/alphabet/<letter>")
 def alphabet(letter):
     ''' Adds letters to the alphabet page '''
-    if letter.len(1) and letter.isalpha():
-        words = mongo.db.dictionary
-        found_words = list(
-            words.find({"category_name": letter.lower()}).sort("word"))
-        print(letter)
-        return render_template(
-            'alphabet.html',
-            page_title=letter,
-            alphabetList=alphabetList,
-            words=found_words)
-    else:
-        return render_template('404.html'), 404
+    words = mongo.db.dictionary
+    found_words = list(
+        words.find({"category_name": letter.lower()}).sort("word"))
+    print(letter)
+    return render_template(
+        'alphabet.html',
+        page_title=letter,
+        alphabetList=alphabetList,
+        words=found_words)
 
 
 @app.route("/register", methods=["GET", "POST"])
